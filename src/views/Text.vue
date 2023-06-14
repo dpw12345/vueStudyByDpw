d7509d544fea0d35abb82ad6ed5554b28039ae4f
dingpeiwen <1529429155@qq.com>
2023-06-14 18:12:37 +0800

    refactor：搭建基本框架

--- src/views/Text.vue
+++ src/views/Text.vue
@@ -7,26 +7,21 @@
             <img src="../assets/robot/robot3.png" alt="">
             <img src="../assets/robot/robot4.png" alt="">
             <img src="../assets/robot/robot5.png" alt="">
-            <!-- <img :src="picRobot" alt=""> -->
-            <!-- <img src="../assets/robot/robot6.png" alt=""> -->
+            <img src="../assets/robot/robot6.png" alt="">
         </li>
     </ul>
 </div>
 </template>
 
 <script>
-import {picRobot} from '../const/pic.js'
 export default {
 data(){
 return {
-    // pic:picRobot[0]
 }
 },
 created(){
-    console.log(this.picRobot);
 },
 mounted(){
-    console.log(this.picRobot);
 },
 methods:{}
 }
